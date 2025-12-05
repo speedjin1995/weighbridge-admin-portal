48592823c2ef37709f272075bf0b4368ab2295c1
speedjin1995 <speedjin1995@gmail.com>
2025-12-05 22:17:36 +0800

    removed unused import

--- src/components/header/UserDropdown.tsx
+++ src/components/header/UserDropdown.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { DropdownItem } from "../ui/dropdown/DropdownItem";
 import { Dropdown } from "../ui/dropdown/Dropdown";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { api } from "../../config/api";
 
 export default function UserDropdown() {
